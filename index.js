ec0dc15b1b1ac9f507ccf672cd4b9784a9c7c250
Daniel <daniel@frytg.com>
2018-10-11 16:15:05 +0200

    0.9.4 fixing a camelCase bug with all uppercase texts plus shorthand methods

--- index.js
+++ index.js
@@ -15,25 +15,38 @@
 */
 
 const yString = {
-	onlyWords: function(str){
+	onlyWords: function(str) {
 		return str.replace(/[^0-9a-zA-Z\xC0-\xFF \-]/g, '');
 	},
 
-	lowerCase: function(str){
+	lowerCase: function(str) {
 		return str.toLowerCase();
 	},
 
-	upperCase: function(str){
+	upperCase: function(str) {
 		return str.toUpperCase();
 	},
 
-	camelCase: function(str){
-		str = helpers.onlyWords(str)
+	camelCase: function(str) {
+		str = yString.onlyWords(str);
+		str = yString.lowerCase(str)
 			.replace(/\-/g, ' ') //convert all hyphens to spaces
-			.replace(/\s[a-z]/g, helpers.upperCase) //convert first char of each word to UPPERCASE
+			.replace(/\s[a-z]/g, yString.upperCase) //convert first char of each word to UPPERCASE
 			.replace(/\s+/g, '') //remove spaces
-			.replace(/^[A-Z]/g, helpers.lowerCase); //convert first char to lowercase
+			.replace(/^[A-Z]/g, yString.lowerCase); //convert first char to lowercase
 		return str;
+	},
+
+	stripWhitespace: function(str) {
+		return str.replace(/\s+/g, '');
+	},
+
+	lc: function(str) {
+		return yString.lowerCase(str);
+	},
+
+	uc: function(str) {
+		return yString.upperCase(str);
 	}
 };
 
