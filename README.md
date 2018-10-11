8eb9ea5fd94bfa4ac66413cab7fcd058db383be6
Daniel <daniel@frytg.com>
2018-10-11 09:01:02 +0200

    Hello world. Init.

---
+++ README.md
@@ -0,0 +1,17 @@
+# yString
+
+### Available methods:
+- `removeNonWord` _(added 0.9.0)_
+- `lowerCase` _(added 0.9.0)_
+- `upperCase` _(added 0.9.0)_
+- `camelCase` _(added 0.9.0)_
+
+
+## LICENSE
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
+
+
+## AUTHOR
+
+- **Daniel Freytag** - [Github](https://github.com/FRYTG) / [Twitter](https://twitter.com/FRYTG)
---
+++ index.js
@@ -0,0 +1,41 @@
+/*
+
+	yString
+
+	GIT			https://github.com/frytg/yString
+
+	DEBUG		-> README.md
+
+	AUTHOR		Daniel Freytag
+				daniel@frytg.com
+				https://twitter.com/FRYTG
+
+	UPDATED		October 2018
+
+*/
+
+const yString = {
+	removeNonWord: function(str){
+		return str.replace(/[^0-9a-zA-Z\xC0-\xFF \-]/g, '');
+	},
+
+	lowerCase: function(str){
+		return str.toLowerCase();
+	},
+
+	upperCase: function(str){
+		return str.toUpperCase();
+	},
+
+	camelCase: function(str){
+		str = helpers.removeNonWord(str)
+			.replace(/\-/g, ' ') //convert all hyphens to spaces
+			.replace(/\s[a-z]/g, helpers.upperCase) //convert first char of each word to UPPERCASE
+			.replace(/\s+/g, '') //remove spaces
+			.replace(/^[A-Z]/g, helpers.lowerCase); //convert first char to lowercase
+		return str;
+	}
+};
+
+
+module.exports = yString;
